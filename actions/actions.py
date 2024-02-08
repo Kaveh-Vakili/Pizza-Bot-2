0f6976a75675fbf049181bb2807ff62569d14119
Kaveh Vakili <vakilik7@gmail.com>
2024-02-07 19:54:09 -0500

    created an action in py file

--- actions/actions.py
+++ actions/actions.py
@@ -5,23 +5,32 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+      return "action_extract_food_entity"
+
+      def run(self, dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+          
+          food_entity=next(tracker.get_latest_entity_values('food'))
+          
+          if food_entity:
+              
+
+            dispatcher.utter_message(text="You have selected {food_entity} as your food choice")
+            
+          else:
+            dispatcher.utter_message(text="I am sorry, I could not detect your food choice")
+
+            return []
+
+
